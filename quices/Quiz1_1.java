2c03120fbdcc35611c73eb2fe207bc60be58e477
valencardenas2901 <141367285+valencardenas2901@users.noreply.github.com>
2023-08-19 20:32:43 -0500

    Quiz1_1.java

--- quices/Quiz1_1.java
+++ quices/Quiz1_1.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Main {
+  public static void main(String[] args) {
+    Scanner leer = new Scanner(System.in);
+
+    int n;
+
+    System.out.println("");
+    n = leer.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("fizzbuzz");
+      } else
+      if (i % 3 == 0) {
+        System.out.println("fizz");
+      } else
+      if (i % 5 == 0) {
+        System.out.println("buzz");
+      } else
+        System.out.println(i);
+
+    }
+  }
+}
